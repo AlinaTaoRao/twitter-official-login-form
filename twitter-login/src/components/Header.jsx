ca88853e3f0e48ce743b05db5b4e6ade653e5fc9
AlinaTaoRao <raotao77@outlook.com>
2022-04-19 19:09:25 +0200

    way 2: props way

--- twitter-login/src/components/Header.jsx
+++ twitter-login/src/components/Header.jsx
@@ -1,16 +1,36 @@
-import './styles.css';
-import { InputForm } from './input-form/InputForm.jsx';
-import { FooterInputForm } from './footer-input-form/FooterInputForm.jsx';
+import "./styles.css";
+import { InputForm } from "./input-form/InputForm.jsx";
+import { FooterInputForm } from "./footer-input-form/FooterInputForm.jsx";
 
+/* way 1, works */
+// export const Header = () => {
+//     return (
+//         <header>
+//         <img src="./assets/logo.png" alt="Logo" />
+//         <InputForm />
+//         <p>or</p>
+//         <FooterInputForm />
+//       </header>
+//     );
+// };
 
-
+/* way 2 : for props*/
+const cssClasses = ["fab fa-apple", "fab fa-windows"];
+const aContents = ["Sign in with Apple", "Sign in with Microsoft"];
 export const Header = () => {
-    return (
-        <header>
-        <img src="./assets/logo.png" alt="Logo" />
-        <InputForm />
-        <p>or</p>
-        <FooterInputForm />
-      </header>
-    );
+  return (
+    <header>
+      <img src="./assets/logo.png" alt="Logo" />
+      {aContents.map((content, index) => (
+        <InputForm
+          key={index}
+          className={cssClasses[index]}
+          content={content}
+        />
+      ))}
+
+      <p>or</p>
+      <FooterInputForm />
+    </header>
+  );
 };
--- twitter-login/src/components/input-form/InputForm.jsx
+++ twitter-login/src/components/input-form/InputForm.jsx
@@ -1,15 +1,28 @@
 import "./styles.css";
 
-/* way 1 */
-export const InputForm = () => {
+/* way 1, works*/
+// export const InputForm = () => {
+//   return (
+//     <div className="input-form">
+//       <a href="#">
+//         <i className="fab fa-apple"></i>Sign in with Apple
+//       </a>
+//       <a href="">
+//         <i className="fab fa-windows"></i>Sign in with Microsoft
+//       </a>
+//     </div>
+//   );
+// };
+
+/* way 2: props way , works*/
+
+export const InputForm = (props) => {
   return (
     <div className="input-form">
       <a href="#">
-        <i className="fab fa-apple"></i>Sign in with Apple
+        <i className={props.class}></i>{props.content}
       </a>
-      <a href="">
-        <i className="fab fa-windows"></i>Sign in with Microsoft
-      </a>
+
     </div>
   );
 };
